01378ab2e83df48749b706343555f3d1045f837f
Tolulope Oluwadamilare Olusakin <oluwadamilare_olusakin@yahoo.com>
2020-03-14 16:51:42 +0100

    Write entity model tests

---
+++ spec/models/entity_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe Entity, type: :model do
+  it { should validate_presence_of(:name) }
+  it { should validate_presence_of(:description) }
+  it { should validate_presence_of(:email) }
+  it { should validate_presence_of(:phone_number) }
+  it { should validate_presence_of(:location) }
+end
