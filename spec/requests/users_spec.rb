bd091a6a3b1f5fb9bf9812d607632af7a1f47ade
Tolulope Oluwadamilare Olusakin <oluwadamilare_olusakin@yahoo.com>
2020-03-14 17:05:02 +0100

    Write users requests tests

---
+++ spec/requests/users_spec.rb
@@ -0,0 +1,89 @@
+require 'rails_helper'
+
+RSpec.describe 'Users API', type: :request do
+  let!(:users) { create_list(:user, 10) }
+  let(:user) { users.first }
+
+
+  describe 'GET /users' do
+    before { get '/users' }
+
+    it 'should return all users' do
+      expect(json.size).to eq(10)
+    end
+
+    it 'should return status of 200' do
+      expect(response).to have_http_status(200)
+    end
+  end
+  
+  describe 'POST /users' do
+    
+    context 'with valid attributes' do
+      before { post '/users', params: valid_user_params }
+
+      it 'should create a user' do
+        expect(json['name']).to eq('The Real Master Yoda')
+      end
+  
+      it 'should return status of 201' do
+        expect(response).to have_http_status(201)
+      end
+    end
+
+    context 'with invalid attributes' do
+      before { post '/users', params: invalid_user_params }
+      it 'should raise an error' do
+        expect(response.body).to match(/Validation failed: Name can't be blank, Email can't be blank/)
+      end
+
+      it 'should return status 422' do
+        expect(response).to have_http_status(422)
+      end
+    end
+    
+  end
+
+  describe 'PUT /users/:id' do
+
+    context 'when the record exists' do
+      before { put "/users/#{user.id}", params: { email: user.email, password: user.password, name: "The Real Yoda" }  }
+      it 'should update a user' do
+        expect(response).to have_http_status(204)
+      end
+  
+    end
+
+    context "when the record doesn't exist" do
+      before { put "/users/300", params: valid_user_params  }
+      it 'should raise an error' do
+        expect(response.body).to match(/Couldn't find User/)
+      end
+
+      it 'should return status 404' do
+        expect(response).to have_http_status(404)
+      end
+    end
+  end
+
+  describe 'DELETE /users/:id' do
+    context 'when record exists' do
+      before { delete "/users/#{user.id}" }
+      
+      it 'should delete the user' do
+        expect(response).to have_http_status((204))
+      end
+    end
+
+    context "when the record doesn't exist" do
+      before { delete "/users/300"}
+      it 'should raise an error' do
+        expect(response.body).to match(/Couldn't find User/)
+      end
+
+      it 'should return status 404' do
+        expect(response).to have_http_status(404)
+      end
+    end
+  end
+end
