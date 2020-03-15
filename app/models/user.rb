4a8593752fc401b4dfc4e8e95959489340e7ea60
Tolulope Oluwadamilare Olusakin <oluwadamilare_olusakin@yahoo.com>
2020-03-15 11:30:28 +0100

    update user model with organization and major assocations and validate first name, last name and user_class

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
-  validates :name, :email, :password, presence: true
+  validates :first_name, :last_name, :email, :user_class,:password, presence: true
   has_secure_password
+  has_many :majorings
+  has_many :majors, through: :majorings
+  has_many :organizations
+  has_many :entities, through: :organizations
 end
