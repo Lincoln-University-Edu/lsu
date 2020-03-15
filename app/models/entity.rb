a48fa19e432f1ea609330c5807d8ff6885eea94a
Tolulope Oluwadamilare Olusakin <oluwadamilare_olusakin@yahoo.com>
2020-03-15 16:48:39 +0100

    Add dependent destroy to organisation on entity

--- app/models/entity.rb
+++ app/models/entity.rb
@@ -1,5 +1,5 @@
 class Entity < ApplicationRecord
   validates :name, :description, :email, :phone_number, :location, presence: true
-  has_many :organisations
+  has_many :organisations, dependent: :destroy
   has_many :users, through: :organisations
 end
