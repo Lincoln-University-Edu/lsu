9759b3f300668014e84d27595b54f6f93d0dadae
Tolulope Oluwadamilare Olusakin <oluwadamilare_olusakin@yahoo.com>
2020-03-15 16:48:11 +0100

    Add dependent destroy to keywording on article

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,5 @@
 class Article < ApplicationRecord
-  has_one :keywording, as: :keywordable
+  has_many :keywording, as: :keywordable, dependent: :destroy
   has_many :keywords, through: :keywording
   validates :author_name, :title, :description, :body, :cover_image, :keywords, :category, presence: true
 end
