78eca98ce9327e4916121556f17b4461a8a1a1ca
Tolulope Oluwadamilare Olusakin <oluwadamilare_olusakin@yahoo.com>
2020-03-15 16:51:55 +0100

    Write getter and setter methods for event_keywords, validate event_keywords, add dependent destroy to keywordings on events

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,16 @@
 class Event < ApplicationRecord
   belongs_to :entity
-  has_one :keywording, as: :keywordable
-  has_many :keywords, through: :keywording
-  validates :datetime, :name, :description, :location, :price, :external_link, :keywords, presence: true
+  has_many :keywordings, as: :keywordable, dependent: :destroy
+  has_many :keywords, through: :keywordings
+  validates :datetime, :name, :description, :location, :price, :external_link, :event_keywords, presence: true
+
+  def event_keywords=(keywords)
+    self.keywords = keywords.split(',').map do |tag|
+      Keyword.where(name: tag.strip).first_or_create!
+    end
+  end
+
+  def event_keywords
+    self.keywords.map(&:name)
+  end
 end
