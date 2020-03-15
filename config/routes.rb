62bba64bd39c793ab46a3b8ae3f27fc4a184920f
Tolulope Oluwadamilare Olusakin <oluwadamilare_olusakin@yahoo.com>
2020-03-15 21:49:40 +0100

    Write routes for events and articles

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :users, only: %i[ index create update destroy ]
-  resources :events, only: %i[ index create update destroy ]
+  resources :events
+  resources :articles, only: %i[ index create update destroy ]
 end
