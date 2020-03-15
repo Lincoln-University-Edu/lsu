80e5e1f3af343dcafff84624dac2c1be964af61c
Tolulope Oluwadamilare Olusakin <oluwadamilare_olusakin@yahoo.com>
2020-03-15 16:52:58 +0100

    Add dependent destroy to majorings on major and change users to students on Major

--- app/models/major.rb
+++ app/models/major.rb
@@ -1,4 +1,4 @@
 class Major < ApplicationRecord
-  has_many :majorings
-  has_many :users, through: :majorings
+  has_many :majorings, dependent: :destroy
+  has_many :students, through: :majorings, class_name: "User"
 end
