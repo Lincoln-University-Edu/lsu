7b35847f98f958dd2e387c0661b4d6ff564ac7a3
Tolulope Oluwadamilare Olusakin <oluwadamilare_olusakin@yahoo.com>
2020-03-15 11:29:03 +0100

    update entity with organistation association

--- app/models/entity.rb
+++ app/models/entity.rb
@@ -1,3 +1,5 @@
 class Entity < ApplicationRecord
   validates :name, :description, :email, :phone_number, :location, presence: true
+  has_many :organisations
+  has_many :users, through: :organisations
 end
