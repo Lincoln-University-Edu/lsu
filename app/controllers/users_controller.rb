a27106465ceec1dc730c11c29122fe24abbc11a4
Tolulope Oluwadamilare Olusakin <oluwadamilare_olusakin@yahoo.com>
2020-03-15 15:29:58 +0100

    Update acceptable user params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,7 +23,7 @@ class UsersController < ApplicationController
 
   private
     def user_params
-      params.permit(:name, :email, :password)
+      params.permit(:first_name, :last_name, :email, :password, :student_class)
     end
 
     def set_user
