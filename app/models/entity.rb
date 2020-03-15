69c845c98ec120c84df236d3f657ab5a9a5e9c58
Tolulope Oluwadamilare Olusakin <oluwadamilare_olusakin@yahoo.com>
2020-03-15 21:47:52 +0100

    Write getter and setter method for keywords on article, add events association to entities

--- app/models/entity.rb
+++ app/models/entity.rb
@@ -2,4 +2,5 @@ class Entity < ApplicationRecord
   validates :name, :description, :email, :phone_number, :location, presence: true
   has_many :organisations, dependent: :destroy
   has_many :users, through: :organisations
+  has_many :events
 end
