367abe53de10fcaca86b8e3c34e56839c3568603
Tolulope Oluwadamilare Olusakin <oluwadamilare_olusakin@yahoo.com>
2020-03-15 16:53:25 +0100

    Add dependent destroy to keywordings on promotion

--- app/models/promotion.rb
+++ app/models/promotion.rb
@@ -1,4 +1,6 @@
 class Promotion < ApplicationRecord
   belongs_to :user
+  has_many :keywordings, as: :keywordable, dependent: :destroy
+  has_many :keywords, through: :keywordings
   validates :title, :description, :email, :phone_number, :price, :category, :condition, presence: true
 end
