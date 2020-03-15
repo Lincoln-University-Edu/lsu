bcd309842b00cda735eff993ee7415a349be6030
Tolulope Oluwadamilare Olusakin <oluwadamilare_olusakin@yahoo.com>
2020-03-15 21:49:09 +0100

    Refactor keyword setter method on event and add keywordable association to Keyword

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,12 +1,12 @@
 class Event < ApplicationRecord
+  validates :datetime, :name, :description, :location, :price, :external_link, :event_keywords, presence: true
   belongs_to :entity
   has_many :keywordings, as: :keywordable, dependent: :destroy
   has_many :keywords, through: :keywordings
-  validates :datetime, :name, :description, :location, :price, :external_link, :event_keywords, presence: true
 
   def event_keywords=(keywords)
-    self.keywords = keywords.split(',').map do |tag|
-      Keyword.where(name: tag.strip).first_or_create!
+    self.keywords = keywords.split(',').map do |keyword|
+      keyword = Keyword.where(name: keyword.strip).first_or_create!
     end
   end
 
--- app/models/keyword.rb
+++ app/models/keyword.rb
@@ -1,4 +1,5 @@
 class Keyword < ApplicationRecord
   has_many :keywordings, dependent: :destroy
+  has_many :keywordables, through: :keywordings
   validates :name, presence: true
 end
