57638d8fb5ffb66e3a0da3a51abaeb4545c18aa4
Tolulope Oluwadamilare Olusakin <oluwadamilare_olusakin@yahoo.com>
2020-03-16 09:18:32 +0100

    Write controller spec helper

---
+++ spec/support/controller_spec_helper.rb
@@ -0,0 +1,9 @@
+module ControllerSpecHelper
+  def token_generator(user_id)
+    JsonWebToken.encode(user_id: user_id)
+  end
+
+  def expired_token_generator(user_id)
+    JsonWebToken.encode({user_id: user_id}, (Time.now.to_i - 20))
+  end
+end
