b59df4019afa8a1b3b698b7119219ad5bd970318
Tolulope Oluwadamilare Olusakin <oluwadamilare_olusakin@yahoo.com>
2020-03-16 17:01:37 +0100

    Add organizations association on entity

--- app/models/entity.rb
+++ app/models/entity.rb
@@ -1,6 +1,6 @@
 class Entity < ApplicationRecord
   validates :name, :description, :email, :phone_number, :location, presence: true
-  has_many :organisations, dependent: :destroy
-  has_many :users, through: :organisations
+  has_many :organizations, dependent: :destroy
+  has_many :users, through: :organizations
   has_many :events
 end
