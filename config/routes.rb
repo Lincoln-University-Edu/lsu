7ce4046e9491b4286678890191c077d0ae977c83
Tolulope Oluwadamilare Olusakin <oluwadamilare_olusakin@yahoo.com>
2020-03-17 10:26:52 +0100

    Write routes for majors, add name validation to majors model tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,18 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  post '/auth/login', to: 'authentication#login'
+  
   resources :users, only: %i[ index create update destroy ]
+
   resources :articles, only: %i[ index create update destroy ]
+
   resources :events, only: %i[ index create update destroy ]
+
   resources :entities, only: %i[ index create update destroy ]
   post '/entities/:id/users/new', to: 'entities#add_user'
   delete '/entities/:id/users/:user_id', to: 'entities#remove_user'
+  
   resources :academic_events, only: %i[ create index update destroy ]
+
+  resources :majors, only: %i[ create index update destroy ]
 end
--- spec/models/major_spec.rb
+++ spec/models/major_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Major, type: :model do
+  it { validate_presence_of(:name) }
   it { should have_many(:majorings) }
   it { should have_many(:students) }
 end
