ab75a541b80d629b4bdbacf6c15ee754b15dee2a
Tolulope Oluwadamilare Olusakin <oluwadamilare_olusakin@yahoo.com>
2020-03-17 10:25:36 +0100

    Write auth controller

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -1,1 +1,6 @@
-m
+class AuthenticationController < ApplicationController
+  def login
+    result = AuthenticateUser.new(params[:email], params[:password]).call
+    json_response(result)
+  end
+end
