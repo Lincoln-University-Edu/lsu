539944d0bd0e54b362d463b54062fc37e96f023e
Tolulope Oluwadamilare Olusakin <oluwadamilare_olusakin@yahoo.com>
2020-03-20 19:12:48 +0100

    Write authorize_request

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,13 @@
 class ApplicationController < ActionController::API
+  before_action :authorize_request
   include ExceptionHandler
   include Response
+
+  attr_reader :current_user
+
+  private
+
+  def authorize_request
+    @current_user = AuthorizeApiRequest.new(request.headers).call[:user]
+  end
 end
