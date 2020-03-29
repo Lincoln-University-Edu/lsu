ce43fbd59841f8f1227f5b41503cd70463678c15
oluwadamilareolusakin <oluwadamilare_olusakin@yahoo.com>
2020-03-29 16:35:10 +0100

    Authorize user based on roles in events controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,8 @@
 class EventsController < ApplicationController
   before_action :set_event, only: %i[ update destroy ]
   before_action :set_entity, only: %i[ create ]
+  before_action :check_new_event_category, only: %i[ create ]
+  before_action :check_existing_event_category, except: %i[ index create ]
 
   def index
    events = Event.all.as_json(methods: :event_category, include: :entity)
@@ -34,5 +36,25 @@ class EventsController < ApplicationController
 
   def event_params
     params.permit( :start_time, :end_time, :name, :description, :location, :price, :external_link, :event_keywords, :entity_id, :event_category)
+  end
+
+  def check_existing_event_category
+    if @event.event_category == 'Student Life'
+      raise(ExceptionHandler::AuthenticationError, "You are not a Student Life Event Publisher") unless current_user.is_student_life_event_publisher
+    end
+
+    if @event.event_category == 'Athletic'
+      raise(ExceptionHandler::AuthenticationError, "You are not an Athletic Event Publisher") unless current_user.is_athletic_event_publisher
+    end
+  end
+
+  def check_new_event_category
+    if params[:event_category] == 'Student Life'
+      raise(ExceptionHandler::AuthenticationError, "You are not a Student Life Event Publisher") unless current_user.is_student_life_event_publisher
+    end
+
+    if params[:event_category] == 'Athletic'
+      raise(ExceptionHandler::AuthenticationError, "You are not an Athletic Event Publisher") unless current_user.is_athletic_event_publisher
+    end
   end
 end
