2164e0fbcc2fa2130836cc4215a4775c55d1e85b
oluwadamilareolusakin <oluwadamilare_olusakin@yahoo.com>
2020-03-29 16:35:57 +0100

    Add roles to user controller params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,10 +24,23 @@ class UsersController < ApplicationController
 
   private
     def user_params
-      params.permit(:first_name, :last_name, :email, :password, :student_class, :all_majors)
+      params.permit(
+        :first_name, 
+        :last_name, 
+        :email, 
+        :password, 
+        :student_class, 
+        :all_majors, 
+        :is_lincolian_press_publisher,
+        :is_student_wire_editor,
+        :is_student_life_event_publisher,
+        :is_academic_event_publisher,
+        :is_athletic_event_publisher,
+      )
     end
 
     def set_user
       @user = User.find(params[:id])
     end
+
 end
