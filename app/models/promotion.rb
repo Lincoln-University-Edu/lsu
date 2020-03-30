49d322063608fb3f552243841f2cb2644ac3d118
oluwadamilareolusakin <oluwadamilare_olusakin@yahoo.com>
2020-03-30 17:57:34 +0100

    Add promotion image urls to promotions

--- app/models/promotion.rb
+++ app/models/promotion.rb
@@ -4,6 +4,8 @@ class Promotion < ApplicationRecord
   has_one :category, through: :categorizing, dependent: :destroy
   has_many :keywordings, as: :keywordable, dependent: :destroy
   has_many :keywords, through: :keywordings
+  has_many :images, as: :imageable, dependent: :destroy
+
   validates :title, :description, :email, :phone_number, :price, :promotion_category, :condition, presence: true
 
   def promotion_category=(category)
@@ -13,6 +15,18 @@ class Promotion < ApplicationRecord
   def promotion_category
     if self.category
       self.category.name
+    end
+  end
+
+  def promotion_image_urls=(urls)
+    self.images = urls.each do |url|
+      Image.where(url: url).first_or_create!
+    end
+  end
+
+  def promotion_image_urls
+    if self.images
+      self.images.map(&:name)
     end
   end
 end
