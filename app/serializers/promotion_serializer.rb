33ecff4c8c89ad6b3f96eb4fad954502cdecaf0a
oluwadamilareolusakin <oluwadamilare_olusakin@yahoo.com>
2020-03-30 17:57:47 +0100

    Add promotion image urls to promotions serializer

--- app/serializers/promotion_serializer.rb
+++ app/serializers/promotion_serializer.rb
@@ -1,5 +1,5 @@
 class PromotionSerializer < ActiveModel::Serializer
-  attributes  :title, :description,:email, :phone_number,:price, :promotion_category, :condition 
+  attributes  :title, :description,:email, :phone_number,:price, :promotion_category, :condition, :promotion_image_urls 
   has_many :keywords
   has_one :category
   belongs_to :user
