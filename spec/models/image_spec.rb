bb3b6d8c3e717b83c9e487f4067b86303c60cf20
oluwadamilareolusakin <oluwadamilare_olusakin@yahoo.com>
2020-03-30 19:22:19 +0100

    Correct image model test and write model tets for imagings

--- spec/models/image_spec.rb
+++ spec/models/image_spec.rb
@@ -1,5 +1,5 @@
 require 'rails_helper'
 
 RSpec.describe Image, type: :model do
-  it { should_validate_presence_of(:url) }
+  it { should validate_presence_of(:url) }
 end
--- spec/models/imaging_spec.rb
+++ spec/models/imaging_spec.rb
@@ -1,5 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Imaging, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should belong_to(:imageable) }
+  it { should belong_to(:image) }
 end
