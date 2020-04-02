3bfbc5186cbbc25329b0bceb9b9c7dfb18541aad
oluwadamilareolusakin <oluwadamilare_olusakin@yahoo.com>
2020-04-02 22:05:34 +0100

    Add uniqueness validations to keywords, entities and majors

--- app/models/entity.rb
+++ app/models/entity.rb
@@ -1,5 +1,6 @@
 class Entity < ApplicationRecord
   validates :name, :description, :email, :phone_number, :location, presence: true
+  validates :name, uniqueness: true
   has_many :organizations, dependent: :destroy
   has_many :users, through: :organizations
   has_many :events
--- app/models/major.rb
+++ app/models/major.rb
@@ -1,5 +1,5 @@
 class Major < ApplicationRecord
-  validates :name, presence: true
+  validates :name, presence: true, uniqueness: true
   has_many :majorings, dependent: :destroy
   has_many :students, through: :majorings, class_name: "User"
 end
