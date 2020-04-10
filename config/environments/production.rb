690ed833d5ca75946bd877e14eeb363c0206c296
oluwadamilareolusakin <oluwadamilare_olusakin@yahoo.com>
2020-04-10 01:54:41 +0100

    Remove web socket url from production environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -34,7 +34,6 @@ Rails.application.configure do
   # Mount Action Cable outside main process or domain.
   config.action_cable.mount_path = '/cable'
   config.action_cable.url = 'wss://api-lsu.herokuapp.com/cable'
-  config.web_socket_server_url = "wss://api-lsu.herokuapp.com/cable" 
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
