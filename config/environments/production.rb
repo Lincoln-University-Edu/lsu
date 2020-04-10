324b57da7287114dabdf0cf56aea9b4dc8416853
oluwadamilareolusakin <oluwadamilare_olusakin@yahoo.com>
2020-04-10 20:21:23 +0100

    Add allowed origin for cable requests

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -34,7 +34,7 @@ Rails.application.configure do
   # Mount Action Cable outside main process or domain.
   config.action_cable.mount_path = '/cable'
   config.action_cable.url = 'wss://api-lsu.herokuapp.com/cable'
-  # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
+  config.action_cable.allowed_request_origins = [ '*']
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   config.force_ssl = true
