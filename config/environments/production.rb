f6b37246a787956d3a68693acce4afebd0ec6c31
oluwadamilareolusakin <oluwadamilare_olusakin@yahoo.com>
2020-04-12 05:09:49 +0100

    Add app origin to allowed origins for actio cable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -34,7 +34,7 @@ Rails.application.configure do
   # Mount Action Cable outside main process or domain.
   config.action_cable.mount_path = '/cable'
   config.action_cable.url = 'wss://api-lsu.herokuapp.com/cable'
-  config.action_cable.allowed_request_origins = [ '*' ]
+  config.action_cable.allowed_request_origins = [ 'https://app.lsu.com' ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   config.force_ssl = true
