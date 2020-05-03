81c5d42467ded196f090298e8e4bf6c8d93eaa59
oluwadamilareolusakin <oluwadamilare_olusakin@yahoo.com>
2020-05-03 23:43:55 +0100

    Add event social accounts to events and write associations for social accounts and social accountings on events

--- app/models/entity.rb
+++ app/models/entity.rb
@@ -6,4 +6,17 @@ class Entity < ApplicationRecord
   has_many :organizations, dependent: :destroy
   has_many :users, through: :organizations
   has_many :events
+
+  has_many :social_accountings, as: :social_accountable
+  has_many :social_accounts, through: :social_accountings
+
+  def event_social_accounts=(accounts)
+    self.social_accounts = accounts.each do |account|
+      SocialAccount.where(type: account[:type], detail: account[:detail]).first_or_create!()
+    end
+  end
+
+  def event_social_accounts
+    self.social_accounts
+  end
 end
