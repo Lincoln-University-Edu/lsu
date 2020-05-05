5b22ae31fe7d3eaab861e1f3deacaf09c68ad704
oluwadamilareolusakin <oluwadamilare_olusakin@yahoo.com>
2020-05-05 18:45:29 +0100

    Add social accounts to entity serializer

--- app/serializers/entity_serializer.rb
+++ app/serializers/entity_serializer.rb
@@ -1,5 +1,7 @@
 class EntitySerializer < ActiveModel::Serializer
-  attributes :id, :name, :description, :email, :phone_number, :location, :is_office, :is_organisation
+  attributes :id, :name, :description, :email, 
+             :phone_number, :location, :is_office, 
+             :is_organisation, :entity_social_accounts
   has_many :users
   has_many :events
 end
