3e1da33f92cd39c019aa6898fd287f5338e088b5
oluwadamilareolusakin <oluwadamilare_olusakin@yahoo.com>
2020-05-08 01:05:40 +0100

    Add conditional to org type variable in full entities

--- app/controllers/entities_controller.rb
+++ app/controllers/entities_controller.rb
@@ -8,7 +8,7 @@ class EntitiesController < ApplicationController
   end
 
   def full_entities
-    org_type = params[:org_type].downcase!
+    org_type = params[:org_type].downcase! if params[:org_type]
 
     if org_type == 'office'
       paginate Entity.office, per_page: 15
